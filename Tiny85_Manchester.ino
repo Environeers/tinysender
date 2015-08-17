8cab56be1e81188bf63dbda518c9c1d089f1a7ae
elnerda <fischkotpf@hotmail.de>
2015-08-17 18:38:12 +0200

    Update Tiny85_Manchester.ino
    mit sleep mode, versendet Daten als 9 byte großes 8 Bit Array

--- Tiny85_Manchester.ino
+++ Tiny85_Manchester.ino
@@ -1,45 +1,121 @@
-//#include <SoftwareSerial.h> //Für debugging
-#include <MANCHESTER.h>
+#include <Manchester.h>
 #include <TinyWireM.h>
-#include <USI_TWI_Master.h>
+#include <USI_TWI_MASTER.h>
 #include <tinybmp.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+/*
+  Prescaler für 433MHz Transmitter und Receiver
+  MAN_300 0
+  MAN_600 1
+  MAN_1200 2
+  MAN_2400 3
+  MAN_4800 4
+  MAN_9600 5
+  MAN_19200 6
+  MAN_38400 7
+  
+     ATTINY84-PU tiny Core Pinout
+               ____
+          VCC-|O   |- GND
+      (0) PB0-|    |- PA0 (10)
+      (1) PB1-|    |- PA1 (9)
+          RST-|    |- PA2 (8)
+      (2) PB2-|    |- PA3 (7)
+      (3) PA7-|    |- PA4/SCL (6)
+  (4) SDA/PA6-|____|- PA5 (5)
 
-//const int rx=3; //Für debugging
-//const int tx=4;//Für debugging
-const int trx=1;
-
+  
+*/
+//Variablen deklarieren
+#define mt 7
+#define BUFFER_SIZE 9
+float Temp;
+uint32_t Pres;
+uint16_t pLow,pHigh;
+uint8_t lLow,hLow,lHigh,hHigh;
+uint8_t a[BUFFER_SIZE];
+//bmp klasse initialisieren
 tinybmp180 tiny;
-//SoftwareSerial ser(rx,tx);//Für debugging
-void setup() {
-  // put your setup code here, to run once:
-tiny.begin();
-//ser.begin(9600);//Für debugging
-MANCHESTER.SetTxPin(trx);
+
+//Interrupt Service Routine für Watchdog timer (ISR)
+ISR(WDT_vect){
+  //erstmal leer weil, muss nur da sein damit der watchdog funktioniert
+  //für jeden interrupt auch eine ISR
 }
 
+void setup() {
+  //Sensor und Transmitter initialisieren
+tiny.begin();
+man.setupTransmit(mt, MAN_1200);
+  //sleep modes initialisieren
+set_sleep_mode(SLEEP_MODE_PWR_DOWN);//nicht das beste zum stromsparen, aber ein anfang
+sleep_enable();
+
+}
+//Union um float in 8 byte zu teilen
+union u_tag {
+   byte b[4]; 
+   float fval;
+ } u;
 void loop() {
-//Temp lesen und in int umwandeln
-float a=tiny.readTemp()*100.0;
-int b =(int)a; 
-//Druck lesen und aus 32 bit in 2 16 bit int machen für besseres senden 
-uint32_t c=tiny.readPressure();
-uint16_t Low=c;
-uint16_t High=c>>16;
-//uint32_t temp=High;//Für debugging
-//temp<<=16;          //Für debugging
-//temp|=Low;//Für debugging
-  // put your main code here, to run repeatedly:
-  MANCHESTER.Transmit(b);
-  delay(20);
-  MANCHESTER.Transmit(Low);
-  delay(20);
-  MANCHESTER.Transmit(High);
-  delay(20);
-  //ser.println(tiny.readPressure()); //Für debugging
-  //ser.println(Low); //Für debugging 
-  //ser.println(High); //Für debugging
-  //ser.println(temp); //Für debugging
-  //ser.println("###############"); //Für debugging
-  //MANCHESTER.Transmit(tiny.readPressure()); //Für debugging
-delay(500);
+  //erstmal schlafen
+//ADCSRA &= ~(1<<ADEN); ADC aussschalten um strom zu sparen, erstmal nicht
+setup_watchdog(5);  // watchdog geht nach 500ms los
+sleep_mode();       // erstmal schlafen, nach 500ms aufwachen
+//ADCSRA |= (1<<ADEN); ADC wieder einschalten
+                    // danach muss die erste messen verworfen werden
+
+
+  //Werte aus dem Sensor auslesen
+Temp=tiny.readTemp();
+Pres=tiny.readPressure();
+//Temperatur and Union für float to byte(4) übergeben
+u.fval=Temp;
+//Luftdruck in 4 ( Bit werte aufteilen
+pLow=Pres;
+pHigh=Pres>>16;
+lLow=pLow;
+hLow=pLow>>8&0xFF;
+lHigh=pHigh;
+hHigh=pHigh>>8&0xFF;
+//Array befüllen:
+a[0]=255;
+a[1]=u.b[0];
+a[2]=u.b[1];
+a[3]=u.b[2];
+a[4]=u.b[3];
+a[5]=lLow;
+a[6]=hLow;
+a[7]=lHigh;
+a[8]=hHigh;
+//Array versenden
+man.transmitArray(9,a);
+}
+//Sets the watchdog timer to wake us up, but not reset
+//0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=250ms, 5=500ms
+//6=1sec, 7=2sec, 8=4sec, 9=8sec
+//From: http://interface.khm.de/index.php/lab/experiments/sleep_watchdog_battery/
+
+void setup_watchdog(int Prescaler){
+  // Kleines Vorwort zu Bit Manipulation in Registern des AVR
+  // &= ~(x<<y) löscht bit x an position y im AVR Register
+  // |= (x<<y) setzt bit x an position y im AVR Register  
+  // _BV steht für Bit Value, kann fürs setzen oder löschen verwendet werden
+  // unabhängig von vorherigen Wert
+
+
+  
+  if(Prescaler>9) Prescaler=9;  //größere Prescaler als 9 gibt es nicht
+                                //deswegen auf 9 limitieren
+  byte bb=Prescaler&7;
+  if(Prescaler>7) bb|=(1<<5);   //das 5 Bit Setzen
+
+//Reihenfolge des Bitsetzens ist wichtig und kann NICHT kombiniert werden
+
+MCUSR &= ~(1<<WDRF);            //Watchdog reset clearen
+WDTCSR |= (1<<WDCE) | (1<<WDE);  // WD_change enable setzen, WD enable setzen
+WDTCSR = bb;                     //neuen watchdog timer wert setzen
+WDTCSR |= _BV(WDIE);             //Interrupt enale setzen, hält den MCU vom resetten beim aufwachen ab
+                                //kann notfalls geändert werden damit er resettet falls notwendig   
 }
