b294fdec77606f16541c3fa3aa6b5ef71fbd3ae4
elnerda <fischkotpf@hotmail.de>
2015-08-17 18:45:37 +0200

    Update UNO_receiver_full_array.ino

--- UNO_receiver_full_array.ino
+++ UNO_receiver_full_array.ino
@@ -24,6 +24,7 @@ void loop() {
   //Daten empfangen:
   if(man.receiveComplete())
   {
+    //zwischenspeicher array um später aus 4 uint8_t eine flaot zu machen
     ub[0]=data[1];
     ub[1]=data[2];
     ub[2]=data[3];
@@ -32,10 +33,10 @@ void loop() {
     hLow=data[6];
     lHigh=data[7];
     hHigh=data[8];
-    newTemp=*(float *)&ub;
-    newLow=(data[6]<<8)|data[5];
-    newHigh=(data[8]<<8)|data[7];
-    newPres=newHigh;
+    newTemp=*(float *)&ub; //c convention die aus dem array ub eine flaot erstellt
+    newLow=(data[6]<<8)|data[5]; //aus den 8 bit variablen 16 bit machen um später 32 bit draus zu machen
+    newHigh=(data[8]<<8)|data[7];//aus den 8 bit variablen 16 bit machen um später 32 bit draus zu machen
+    newPres=newHigh; //aus 16 bit 32 bit machen, kann man leider nicht auf einmal machen, ka warum
     newPres<<=16;
     newPres|=newLow;
     Serial.println(newPres);
